1ceffed3099b822124dee956c6abb8ff2a1b8437
tan-nhat <cvqdvnt899@gmail.com>
2025-08-20 01:45:03 +0700

    Init commit

---
+++ map/map.js
@@ -0,0 +1,27 @@
+
+const people = [
+    {name: 'bob', age: 20, position: 'developer'},
+    {name: 'peter', age: 25, position: 'designer'},
+    {name: 'susy', age: 30, position: 'the boss'},
+    {name: 'ana', age: 35, position: 'the boss'},
+];
+
+const ages = people.map(function(person) {
+    return person.age + 20;
+});
+
+const newPeople = people.map(function(person) {
+    return {
+        firstName:person.name.toUpperCase(),
+        oldAge:person.age + 20,
+    }
+});
+
+//DOM
+const names = people.map(function(person){
+    return `<h1>${person.name}</h1>`
+})
+
+//document.body.innerHTML = names.join('');
+
+console.log(newPeople);
