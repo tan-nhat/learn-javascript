a093f421a616d53a6c480bee5edfa1862313a3cd
Nhật Tân Võ <vonhattan.work01@gmai.com>
2025-09-01 01:51:19 +0700

    add random number generation logic to Math Object

---
+++ Math Object/index.js
@@ -0,0 +1,22 @@
+// const number = 4.56789;
+// const result = Math.floor(number);
+
+// const number = 4.12222;
+// const result = Math.ceil(number);
+
+// const number = 25;
+// const result = Math.sqrt(number);
+
+// const result = Math.PI;
+
+// const result = Math.min(4,5,6,7,8,9);
+
+// const result = Math.max(4,5,6,7,8,9,100,200,1000);
+
+// const result = Math.random();
+
+// Tạo số random với 10 chữ số
+const result = Math.floor(Math.random() * 10 + 1);
+
+
+console.log(result);
